0ba1f94cb2e7087ac937a1b175b187413af2814b
Robert <mr.retoko@mail.ru>
2020-12-24 22:23:49 +0300

    Revert "add layout calculators"
    This reverts commit 2b4142f34cb203f37b73038469d4285732e0b3ae.

--- app/src/main/java/ru/android/study/ui/movies_details/adapters/ActorsListAdapter.kt
+++ app/src/main/java/ru/android/study/ui/movies_details/adapters/ActorsListAdapter.kt
@@ -1,5 +1,7 @@
 package ru.android.study.ui.movies_details.adapters
 
+import android.content.Context
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,9 +14,9 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import ru.android.study.R
 import ru.android.study.data.model.Actor
-import ru.android.study.utils.calculateActorsHolderWidth
 
 class ActorsListAdapter : RecyclerView.Adapter<ActorViewHolder>() {
+
   private var actors = listOf<Actor>()
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder {
@@ -24,6 +26,21 @@ class ActorsListAdapter : RecyclerView.Adapter<ActorViewHolder>() {
     itemView.layoutParams.width = calculateActorsHolderWidth(parent.context, 4)
 
     return ActorViewHolder(itemView)
+  }
+
+  private fun calculateActorsHolderWidth(context: Context, actorsCount: Int): Int {
+    val displayMetrics: DisplayMetrics = context.resources.displayMetrics
+    val screenWidth = displayMetrics.widthPixels
+    val moviesDetailsMarginsCount = 2 // left and right
+    val moviesDetailsMarginHorizontal =
+      context.resources.getDimension(R.dimen.movies_details_margin_horizontal)
+        .times(moviesDetailsMarginsCount)
+    val actorsMarginCount = actorsCount - 1 // right margin between items = n-1
+    val marginBetweenActors =
+      context.resources.getDimension(R.dimen.movie_details_actors_margin_horizontal)
+        .times(actorsMarginCount)
+    val marginsTotal = marginBetweenActors.plus(moviesDetailsMarginHorizontal)
+    return screenWidth.minus(marginsTotal).div(actorsCount).toInt()
   }
 
   override fun onBindViewHolder(holder: ActorViewHolder, position: Int) {
